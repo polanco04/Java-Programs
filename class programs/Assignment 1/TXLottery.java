4f83e309b485328f372d25fda212c62d9ee787de
polanco04 <156978928+polanco04@users.noreply.github.com>
2024-09-12 17:10:16 -0500

    assignment done (might add stuff)

---
+++ class programs/Assignment 1/TXLottery.java
@@ -0,0 +1,143 @@
+// Carlos Polanco
+// This program is meant to simulate the Texas Lottery. The user enters 6 
+// numbers between 1 and 54 (inclusive) and see if it matches with the
+// 6 randomly generated numbers. Numbers cannot be outside the range
+// and must be unique, meaning no repeated numbers.
+
+
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+
+class TXLottery
+{
+    public static void printHeading()
+    {
+        System.out.println("Carlos Polanco");
+        System.out.print("""
+                         This program is meant to simulate the Texas Lottery.
+                         The user enters numbers between 1 and 54 (inclusive)
+                         and see if it matches with the 6 randomly generated 
+                         numbers. Numbers cannot be outside the range and 
+                         must be unique, meaning no repeated numbers. \n\n""");
+    }
+  
+    public static void randNum(int[] lot)
+    {
+      Random lotNum = new Random();
+      
+      // creating a set to store the numbers before storing them in an array.
+      // this is to make sure that the numbers being added are unique.
+      // add() does not let any duplicates into the set.
+      HashSet<Integer> randNums = new HashSet<>();
+
+      while(randNums.size() < lot.length)
+      {
+        int rNum = lotNum.nextInt(1,55);
+        randNums.add(rNum);
+      }  
+      
+      Integer randArr[] = randNums.toArray(new Integer[lot.length]);
+     
+     // copying this way due to randArr being considered an array of objects.
+     // could probably use casting as well. 
+     for(int i = 0; i < lot.length; i++)
+      {
+        lot[i] = randArr[i];
+      }
+      
+    }
+
+    public static void playerNums(int[] plot)
+    {
+       int numbers;
+       Scanner nums = new Scanner(System.in);
+
+       // using a set again since it includes the contains() method
+       // which checks if a number that was entered is in the list/set.
+       HashSet<Integer> userNums = new HashSet<>();
+       
+       System.out.println("Welcome to the Texas Lottery!");
+       
+       // while loop to ask the user for their numbers until the size
+       // of the set gets to 6 (length of the lottery digits array)
+       while(userNums.size() < plot.length) 
+       {
+         System.out.println("Please enter a number between 1 and 54: ");
+         numbers = nums.nextInt();
+        
+        // if the number is already in the set, ask the user again.
+        if(userNums.contains(numbers))
+        {
+          while(numbers < 1 || numbers > 54 || userNums.contains(numbers))
+          {
+            System.out.println("Please try again. Your number must be unique and in range.");
+            numbers = nums.nextInt();
+          }
+          userNums.add(numbers);
+        }
+        
+        // if the number is out of range, ask the user again.
+        else if(numbers < 1 || numbers > 54)
+        {
+          System.out.println("Please try again. Your number must be between 1 and 54.");
+        }
+
+        // if the number is valid, add it to the set.
+        else
+        {
+          userNums.add(numbers);
+        }
+       }
+      
+      Integer userArr[] = userNums.toArray(new Integer[plot.length]);
+      
+      // Same as the for loop in the random array.
+      // cant use copyOf or arraycopy due to userArr being an array of objects.
+      // could use casting instead and then use copyOf or arraycopy.
+      for(int i = 0; i < plot.length; i++)
+      {
+        plot[i] = userArr[i];
+      }
+    }
+
+    public static int matchingNums(int[] lot, int[] plot)
+    {
+       int matching = 0;
+
+       // goes through and checks if there are any matching numbers.
+       for(int i = 0; i < plot.length; i++)
+       {
+          if(lot[i] == plot[i])
+          {
+            matching++;
+          }
+       }
+       return matching;
+    }
+
+    public static void printArrays(int[] arr)
+    {
+      // loop to print the array.
+      for(int i = 0; i < arr.length; i++)
+      {
+         System.out.print(arr[i] + " ");
+      }
+      System.out.println();
+    }
+    public static void main(String [] args)
+    {
+      // arrays.
+      int userNums[] = new int[6];
+      int lotteryNums[] = new int[6];
+
+      //function/method calls. 
+      printHeading();
+      randNum(lotteryNums);
+      playerNums(userNums);
+      System.out.println("You matched: " + matchingNums(lotteryNums, userNums) + " numbers.");
+      printArrays(userNums);
+      printArrays(lotteryNums);
+    }
+}
